659084a334b12613ffb1490abe3335b38d0a86f6
Stephanie Asmar <sasmar8@gmail.com>
2017-08-13 21:46:25 -0400

    created gitignore file; modifies html, css and js files to begin process of creating hamburger menu for navbar

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,63 @@
+//---HAMBURGER MENU---//
 
-//define a function that 
+// Initializes all functions needed for page after document loads
+function hideAboutElements() {
+    $SHOW_MAP.hide();
+    $EXIT_ICON.hide();
+    $MENU_CONTAINER.hide();
+    $(".click-to-close").hide();
+}
+
+function addAboutListeners() {
+    clickMenuShow();
+    clickExitButton();
+    clickHideMap();
+    clickShowMap();
+}
+// allows document to load before functions are called
+$(document).ready(function() {
+    hideAboutElements();
+    addAboutListeners();
+});
+
+
+
+
+
+
+//---LOTTERY FUNCTIONALITY---//
+
+//define a function that randomly generates a number from 1-69:
 function randomFiveNumbers() {
     var fiveNumberArray = [];
 
-    for(var i=0; i<+4; i++) {
+    for(var i=0; i<=4; i++) {
         fiveNumberArray.push(Math.floor(Math.random() * 69))
     }
-    return ("Your 5 numbers are: " + fiveNumberArray.join(" "));
+    return ("Your 5 numbers are: " + fiveNumberArray.join(", "));
 }
 
-function randomTwoNumbers() {
-    var twoNumberArray = [];
-
-    for(var i=0; i<=1; i++) {
-        ftwoNumberArray.push(Math.floor(Math.random() * 26))
-    }
-    return ("Your 2 numbers are: " + twoNumberArray.join(" "));
+//Access the button:
+function getButton() {
+    var button = document.querySelector(".pickNumberButton");
 }
+
+//Access the lottery box:
+function getLottoBox() {
+    var lottoBox = document.querySelector(".lotteryNumberContainer");
+    getLotoBox.textContent = randomFiveNumbers();
+}
+
+//Create function that causes thing to happen when button is clicked:
+function clickButton() {
+    var clickMe = getButton();
+    clickMe.addEventListener("click", function (event) {
+        event.preventDefault();
+});
+
+};
+
+clickButton();
 
 
 
